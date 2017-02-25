29ce1ff63bd17202da5399fd90595a589b05c898
Rechlone <vinicius-rech@hotmail.com>
2017-02-25 18:04:41 -0300

    base

---
+++ home.php
@@ -0,0 +1,341 @@
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/css/style.css" />
+<body>
+    <header>
+        <img src="assets/images/interface/logo.png" class="logo" />
+        <nav class="navigation">
+            <ul>
+                <li>Guerreiro</li>
+                <li>Templária</li>
+                <li>Feiticeiro</li>
+                <li>Clériga</li>
+                <li>Atirador</li>
+                <li>Dual</li>
+            </ul>
+        </nav>
+        <span class="login-panel">Login/Registrar</span>
+        <video id="video" class="video" muted loop autoplay>
+        <source src="assets/videos/r.mp4" type="video/mp4">
+        <source src="assets/videos/r.webm" type="video/webm">
+        </video>
+    </header>
+    <div class="container">
+        <section class="post">
+            <div class="imgs">
+                <img src="assets/images/1/1/1.png" />
+                <img src="assets/images/1/2/1.png" />
+                <img src="assets/images/1/3/1.png" />
+                <img src="assets/images/1/4/1.png" />
+                <img src="assets/images/1/5/1.png" />
+            </div>
+            <div class="text">
+                <span class="text">Vendo ou troco por itens de dual com a mesma refinação</span><br />
+                <span class="text-data">Anunciado por <a href="#">Admin</a> em 22/02/2017 as 17:46:35</span>
+            </div>
+            <div class="post-stats">
+                <ul>
+                    <li>visualizações</li>
+                    <li>215</li>
+                </ul>
+                <ul>
+                    <li>ofertas</li>
+                    <li>28</li>
+                </ul>
+                <ul>
+                    <li>Nação</li>
+                    <li>Selest</li>
+                </ul>
+                <ul>
+                    <li>Servidor</li>
+                    <li>Arcan</li>
+                </ul>
+            </div>
+        </section>
+        <section class="post">
+            <div class="imgs">
+                <img src="assets/images/1/1/1.png" />
+                <img src="assets/images/1/2/1.png" />
+                <img src="assets/images/1/3/1.png" />
+                <img src="assets/images/1/4/1.png" />
+                <img src="assets/images/1/5/1.png" />
+            </div>
+            <div class="text">
+                <span class="text">Vendo ou troco por itens de dual com a mesma refinação</span><br />
+                <span class="text-data">Anunciado por <a href="#">Admin</a> em 22/02/2017 as 17:46:35</span>
+            </div>
+            <div class="post-stats">
+                <ul>
+                    <li>215</li>
+                    <li>visualizações</li>
+                </ul>
+                <ul>
+                    <li>28</li>
+                    <li>ofertas</li>
+                </ul>
+                <ul>
+                    <li>Selest</li>
+                    <li>nação</li>
+                </ul>
+                <ul>
+                    <li>Arcan</li>
+                    <li>Servidor</li>
+                </ul>
+            </div>
+        </section>
+        <section class="post">
+            <div class="imgs">
+                <img src="assets/images/1/1/1.png" />
+                <img src="assets/images/1/2/1.png" />
+                <img src="assets/images/1/3/1.png" />
+                <img src="assets/images/1/4/1.png" />
+                <img src="assets/images/1/5/1.png" />
+            </div>
+            <div class="text">
+                <span class="text">Vendo ou troco por itens de dual com a mesma refinação</span><br />
+                <span class="text-data">Anunciado por <a href="#">Admin</a> em 22/02/2017 as 17:46:35</span>
+            </div>
+            <div class="post-stats">
+                <ul>
+                    <li>215</li>
+                    <li>visualizações</li>
+                </ul>
+                <ul>
+                    <li>28</li>
+                    <li>ofertas</li>
+                </ul>
+                <ul>
+                    <li>Selest</li>
+                    <li>nação</li>
+                </ul>
+                <ul>
+                    <li>Arcan</li>
+                    <li>Servidor</li>
+                </ul>
+            </div>
+        </section>
+        <section class="post">
+            <div class="imgs">
+                <img src="assets/images/1/1/1.png" />
+                <img src="assets/images/1/2/1.png" />
+                <img src="assets/images/1/3/1.png" />
+                <img src="assets/images/1/4/1.png" />
+                <img src="assets/images/1/5/1.png" />
+            </div>
+            <div class="text">
+                <span class="text">Vendo ou troco por itens de dual com a mesma refinação</span><br />
+                <span class="text-data">Anunciado por <a href="#">Admin</a> em 22/02/2017 as 17:46:35</span>
+            </div>
+            <div class="post-stats">
+                <ul>
+                    <li>215</li>
+                    <li>visualizações</li>
+                </ul>
+                <ul>
+                    <li>28</li>
+                    <li>ofertas</li>
+                </ul>
+                <ul>
+                    <li>Selest</li>
+                    <li>nação</li>
+                </ul>
+                <ul>
+                    <li>Arcan</li>
+                    <li>Servidor</li>
+                </ul>
+            </div>
+        </section>
+        <section class="post">
+            <div class="imgs">
+                <img src="assets/images/1/1/1.png" />
+                <img src="assets/images/1/2/1.png" />
+                <img src="assets/images/1/3/1.png" />
+                <img src="assets/images/1/4/1.png" />
+                <img src="assets/images/1/5/1.png" />
+            </div>
+            <div class="text">
+                <span class="text">Vendo ou troco por itens de dual com a mesma refinação</span><br />
+                <span class="text-data">Anunciado por <a href="#">Admin</a> em 22/02/2017 as 17:46:35</span>
+            </div>
+            <div class="post-stats">
+                <ul>
+                    <li>215</li>
+                    <li>visualizações</li>
+                </ul>
+                <ul>
+                    <li>28</li>
+                    <li>ofertas</li>
+                </ul>
+                <ul>
+                    <li>Selest</li>
+                    <li>nação</li>
+                </ul>
+                <ul>
+                    <li>Arcan</li>
+                    <li>Servidor</li>
+                </ul>
+            </div>
+        </section>
+        <section class="post">
+            <div class="imgs">
+                <img src="assets/images/1/1/1.png" />
+                <img src="assets/images/1/2/1.png" />
+                <img src="assets/images/1/3/1.png" />
+                <img src="assets/images/1/4/1.png" />
+                <img src="assets/images/1/5/1.png" />
+            </div>
+            <div class="text">
+                <span class="text">Vendo ou troco por itens de dual com a mesma refinação</span><br />
+                <span class="text-data">Anunciado por <a href="#">Admin</a> em 22/02/2017 as 17:46:35</span>
+            </div>
+            <div class="post-stats">
+                <ul>
+                    <li>215</li>
+                    <li>visualizações</li>
+                </ul>
+                <ul>
+                    <li>28</li>
+                    <li>ofertas</li>
+                </ul>
+                <ul>
+                    <li>Selest</li>
+                    <li>nação</li>
+                </ul>
+                <ul>
+                    <li>Arcan</li>
+                    <li>Servidor</li>
+                </ul>
+            </div>
+        </section>
+        <section class="post">
+            <div class="imgs">
+                <img src="assets/images/1/1/1.png" />
+                <img src="assets/images/1/2/1.png" />
+                <img src="assets/images/1/3/1.png" />
+                <img src="assets/images/1/4/1.png" />
+                <img src="assets/images/1/5/1.png" />
+            </div>
+            <div class="text">
+                <span class="text">Vendo ou troco por itens de dual com a mesma refinação</span><br />
+                <span class="text-data">Anunciado por <a href="#">Admin</a> em 22/02/2017 as 17:46:35</span>
+            </div>
+            <div class="post-stats">
+                <ul>
+                    <li>215</li>
+                    <li>visualizações</li>
+                </ul>
+                <ul>
+                    <li>28</li>
+                    <li>ofertas</li>
+                </ul>
+                <ul>
+                    <li>Selest</li>
+                    <li>nação</li>
+                </ul>
+                <ul>
+                    <li>Arcan</li>
+                    <li>Servidor</li>
+                </ul>
+            </div>
+        </section>
+        <section class="post">
+            <div class="imgs">
+                <img src="assets/images/1/1/1.png" />
+                <img src="assets/images/1/2/1.png" />
+                <img src="assets/images/1/3/1.png" />
+                <img src="assets/images/1/4/1.png" />
+                <img src="assets/images/1/5/1.png" />
+            </div>
+            <div class="text">
+                <span class="text">Vendo ou troco por itens de dual com a mesma refinação</span><br />
+                <span class="text-data">Anunciado por <a href="#">Admin</a> em 22/02/2017 as 17:46:35</span>
+            </div>
+            <div class="post-stats">
+                <ul>
+                    <li>215</li>
+                    <li>visualizações</li>
+                </ul>
+                <ul>
+                    <li>28</li>
+                    <li>ofertas</li>
+                </ul>
+                <ul>
+                    <li>Selest</li>
+                    <li>nação</li>
+                </ul>
+                <ul>
+                    <li>Arcan</li>
+                    <li>Servidor</li>
+                </ul>
+            </div>
+        </section>
+        <section class="post">
+            <div class="imgs">
+                <img src="assets/images/1/1/1.png" />
+                <img src="assets/images/1/2/1.png" />
+                <img src="assets/images/1/3/1.png" />
+                <img src="assets/images/1/4/1.png" />
+                <img src="assets/images/1/5/1.png" />
+            </div>
+            <div class="text">
+                <span class="text">Vendo ou troco por itens de dual com a mesma refinação</span><br />
+                <span class="text-data">Anunciado por <a href="#">Admin</a> em 22/02/2017 as 17:46:35</span>
+            </div>
+            <div class="post-stats">
+                <ul>
+                    <li>215</li>
+                    <li>visualizações</li>
+                </ul>
+                <ul>
+                    <li>28</li>
+                    <li>ofertas</li>
+                </ul>
+                <ul>
+                    <li>Selest</li>
+                    <li>nação</li>
+                </ul>
+                <ul>
+                    <li>Arcan</li>
+                    <li>Servidor</li>
+                </ul>
+            </div>
+        </section>
+        <section class="post">
+            <div class="imgs">
+                <img src="assets/images/1/1/1.png" />
+                <img src="assets/images/1/2/1.png" />
+                <img src="assets/images/1/3/1.png" />
+                <img src="assets/images/1/4/1.png" />
+                <img src="assets/images/1/5/1.png" />
+            </div>
+            <div class="text">
+                <span class="text">Vendo ou troco por itens de dual com a mesma refinação</span><br />
+                <span class="text-data">Anunciado por <a href="#">Admin</a> em 22/02/2017 as 17:46:35</span>
+            </div>
+            <div class="post-stats">
+                <ul>
+                    <li>215</li>
+                    <li>visualizações</li>
+                </ul>
+                <ul>
+                    <li>28</li>
+                    <li>ofertas</li>
+                </ul>
+                <ul>
+                    <li>Selest</li>
+                    <li>nação</li>
+                </ul>
+                <ul>
+                    <li>Arcan</li>
+                    <li>Servidor</li>
+                </ul>
+            </div>
+        </section>
+        <nav>
+        </nav>
+
+
+    </div>
+</body>
+</html>
